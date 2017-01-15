42914924423b89a603f5ef8850410b18c03526c1
Michal Dziekonski <glibnes@gmail.com>
2017-01-15 01:15:43 +0100

    Add no implementation error

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <vector>
 #include <string>
 
-int main(int argc, char** argv) {
+#include "./modules/error-handler/ErrorHandler.hpp"
+
+using ErrorHandler = pobr::modules::ErrorHandler;
+
+int main(int argc, char** argv)
+{
     std::vector<std::string> arguments(argv + 1, argv + argc);
 
     // TODO: Implement application
     //       pass args from "arguments" variable
+
+    auto errHandler = ErrorHandler();
+
+    errHandler.error("Nothing implemented yet", false);
 
     return 0;
 }
