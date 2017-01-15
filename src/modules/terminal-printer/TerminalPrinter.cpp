13f42e880cb4274e910f7cfa0821612854f4f5eb
Michal Dziekonski <glibnes@gmail.com>
2017-01-15 01:07:57 +0100

    Add TerminalPrinter class

---
+++ src/modules/terminal-printer/TerminalPrinter.cpp
@@ -0,0 +1,54 @@
+#include "TerminalPrinter.hpp"
+
+#include <sstream>
+#include <iostream>
+
+using TerminalPrinter = pobr::modules::TerminalPrinter;
+
+const std::map<std::string, const unsigned int>&
+TerminalPrinter::getColors()
+{
+    static const std::map<std::string, const unsigned int> colors = {
+        { "red", 31 },
+        { "yellow", 33 },
+        { "blue", 34 },
+        { "cyan", 36 },
+        { "magenta", 35 }
+    };
+
+    return colors;
+}
+
+const std::string
+TerminalPrinter::colorize(const std::string& message, const std::string& color)
+{
+    if (POBR_CONFIG_TERMINALCOLORS && TerminalPrinter::getColors().count(color) == 1)
+    {
+        std::stringstream temp;
+
+        temp << "\e[" << TerminalPrinter::getColors().at(color) << "m"
+             << message
+             << "\e[0m";
+
+        return temp.str();
+    }
+
+    return message;
+}
+
+void
+TerminalPrinter::printLabel(const std::string& message, const std::string& color)
+{
+    std::stringstream temp;
+
+    temp << "[" << message << "]";
+
+    std::cout << TerminalPrinter::colorize(temp.str(), color)
+              << " ";
+}
+
+const unsigned int
+TerminalPrinter::getLabelLength(const std::string& message)
+{
+    return message.length() + 3;
+}
---
+++ src/modules/terminal-printer/TerminalPrinter.hpp
@@ -0,0 +1,24 @@
+#ifndef POBR_MODULES_TERMINALPRINTER_HPP
+#define POBR_MODULES_TERMINALPRINTER_HPP
+
+#ifndef POBR_CONFIG_TERMINALCOLORS
+#define POBR_CONFIG_TERMINALCOLORS true
+#endif
+
+#include <string>
+#include <map>
+
+namespace pobr { namespace modules
+{
+    class TerminalPrinter
+    {
+    protected:
+        static const std::map<std::string, const unsigned int>& getColors();
+
+        static const std::string colorize(const std::string& message, const std::string& color);
+        static void printLabel(const std::string& message, const std::string& color = "");
+        static const unsigned int getLabelLength(const std::string& message);
+    };
+}}
+
+#endif
