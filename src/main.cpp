89ce5ead0f2f19c051a4974f3211031041c27a40
Michal Dziekonski <glibnes@gmail.com>
2017-01-15 01:01:32 +0100

    Add src/ directory with initial main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+int main(int argc, char** argv) {
+    std::vector<std::string> arguments(argv + 1, argv + argc);
+
+    // TODO: Implement application
+    //       pass args from "arguments" variable
+
+    return 0;
+}
