52eab61e041d0b22f6a5d8342e42f80d41c954c3
Michal Dziekonski <glibnes@gmail.com>
2017-01-21 00:25:02 +0100

    Create constants namespace

---
+++ src/utils/consts.hpp
@@ -0,0 +1,11 @@
+#ifndef POBR_UTILS_CONSTS_HPP
+#define POBR_UTILS_CONSTS_HPP
+
+#include <cstdint>
+
+namespace pobr::utils::consts::colors {
+    const uint8_t white = 255;
+    const uint8_t black = 0;
+}
+
+#endif
